374ddd3dbc62233f3c367cc859fcfd9e7f873a8f
kythinhle <kythinhle@gmail.com>
2020-12-28 14:52:21 +0700

    Completed - validate order status based on Pace transaction

--- upload/admin/language/en-gb/extension/payment/pace_checkout.php
+++ upload/admin/language/en-gb/extension/payment/pace_checkout.php
@@ -1,23 +1,26 @@
 <?php
 // Heading
-$_['heading_title']      = 'Pace checkout';
+$_['heading_title']                            = 'Pace Checkout';
 
 // Text
-$_['text_extension']     = 'Extensions';
-$_['text_success']       = 'Success: You have modified Pace checkout payment module!';
-$_['text_edit']          = 'Edit Pace checkout';
+$_['text_extension']                           = 'Extensions';
+$_['text_success']                             = 'Success: You have modified Pace checkout payment module!';
+$_['text_edit']                                = 'Edit Pace checkout';
+$_['text_pace_checkout']                       = '<img src="view/image/payment/pace.png" alt="Paymate" title="Pace Payment" width="95px" />';
 
 // Entry
-$_['entry_total']        = 'Total';
-$_['entry_order_status'] = 'Order Status';
-$_['entry_geo_zone']     = 'Geo Zone';
-$_['entry_status']       = 'Enable';
-$_['enable_sandbox']     = 'Enable Playground';
-$_['pay_mode_title']     = 'Pay With Pace Mode';
-$_['entry_sort_order']   = 'Sort Order';
+$_['entry_total']                              = 'Total';
+$_['entry_order_status']                       = 'Order Status';
+$_['entry_order_status_transaction_cancelled'] = 'Order status when Pace transaction is cancelled';
+$_['entry_order_status_transaction_expired']   = 'Order status when Pace transaction has expired';
+$_['entry_geo_zone']                           = 'Geo Zone';
+$_['entry_status']                             = 'Enable';
+$_['enable_sandbox']                           = 'Enable Playground';
+$_['pay_mode_title']                           = 'Pay With Pace Mode';
+$_['entry_sort_order']                         = 'Sort Order';
 
 // Help
-$_['help_total']         = 'The checkout total the order must reach before this payment method becomes active.';
+$_['help_total']                               = 'The checkout total the order must reach before this payment method becomes active.';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment Cash On Delivery!';
+$_['error_permission']                         = 'Warning: You do not have permission to modify payment Cash On Delivery!';
